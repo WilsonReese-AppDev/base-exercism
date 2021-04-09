675e2e6a263697fede0690d06fc7e51002b03d9b
WilsonReese <reese@chicagobooth.edu>
2021-04-09 03:02:58 +0000

    Clean main branch

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -5,13 +5,3 @@ Write your code for the 'Acronym' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/acronym` directory.
 =end
-
-class Acronym
-  def self.abbreviate(a_string)
-    words = a_string.split(" ")
-    abbrev = ""
-    words.each do |a_word|
-      abbrev += a_word[0].upcase
-    end
-  end
-end
