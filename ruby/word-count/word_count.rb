2bf6f071502aef90922019783c0892a0efb2a078
WilsonReese <reese@chicagobooth.edu>
2021-04-09 03:41:03 +0000

    does this work

---
+++ ruby/word-count/word_count.rb
@@ -0,0 +1,29 @@
+=begin
+Write your code for the 'Word Count' exercise in this file. Make the tests in
+`word_count_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/word-count` directory.
+=end
+
+class WordCount
+    def word_count(phrase)
+        words = text.downcase.split(/\W+/)
+        # number_of_words = Hash.new
+        # words.each do |word|
+        #     number_of_words[word] = words.count(word)
+        # end
+        counts = words.tally
+        # p "```text"
+        # number_of_words.each_key do
+        #     number_of_words(key).to_s + ": " + number_of_words[key].to_s
+        # end
+        # p "````"
+    end
+end
+
+# olly: 2
+# in: 1
+# come: 1
+# free: 1
+# ```
