dfe2550089363d5ac2eacb624dc3b74d5922314b
WilsonReese <reese@chicagobooth.edu>
2021-04-10 18:24:32 +0000

    finished_matrix_test_space

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -6,3 +6,20 @@ To get started with TDD, see the `README.md` file in your
 `ruby/matrix` directory.
 =end
 
+class Matrix 
+    def initialize(string)
+        @array_of_rows = string.split(/\n/)
+    end
+
+    def rows
+        matrix = []
+        @array_of_rows.each {|row| matrix << row.split(/\W/).map(&:to_i)}
+        matrix
+    end
+
+    def columns
+        matrix = []
+            @array_of_rows.each {|row| matrix << row.split(/\W/).map(&:to_i)}
+        matrix.transpose
+    end
+end
