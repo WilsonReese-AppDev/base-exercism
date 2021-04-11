8dd766de7ec26635cb08c27d91a8e68d1904fa10
WilsonReese <reese@chicagobooth.edu>
2021-04-11 18:24:11 +0000

    finished twelve days

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -7,26 +7,11 @@ To get started with TDD, see the `README.md` file in your
 =end
 
 class TwelveDays 
-    def base_song(day = "first")
+    def self.base_song(day = "first")
         @song = "On the #{day} day of Christmas my true love gave to me: "
     end
 
-    def song
-        # lyrics = {first => "a Partridge in a Pear Tree",
-        #           second => "two Turtle Doves",
-        #           third => "three French Hens",
-        #           fourth => "four Calling Birds",
-        #           fifth => "five Gold Rings",
-        #           sixth => "six Geese-a-Laying",
-        #           seventh => "seven Swans-a-Swimming",
-        #           eighth => "eight Maids-a-Milking",
-        #           ninth => "nine Ladies Dancing",
-        #           tenth => "ten Lords-a-Leaping",
-        #           eleventh => "eleven Pipers Piping",
-        #           twelfth => "twelve Drummers Drumming"}
-
-        # lyrics.each do |day|
-            
+    def self.song            
         ordinals = ["first","second","third","fourth","fifth","sixth","seventh","eighth","ninth","tenth","eleventh","twelfth"]
         gifts = ["a Partridge in a Pear Tree",
                  "two Turtle Doves",
@@ -50,25 +35,35 @@ class TwelveDays
                     daily_gifts[-1] = daily_gifts[-1].prepend("and ")
                 end
             end
-            verses[day - 1] = daily_gifts.join(", ")
-            # daily_gifts.each do |gift|
+            verses[day - 1] = base_song(ordinals[day-1]) + daily_gifts.join(", ") + "."
+
+        end
+        verses.join("\n\n") + "\n"
+    end
+    
+end
+
+
+        # lyrics = {first => "a Partridge in a Pear Tree",
+        #           second => "two Turtle Doves",
+        #           third => "three French Hens",
+        #           fourth => "four Calling Birds",
+        #           fifth => "five Gold Rings",
+        #           sixth => "six Geese-a-Laying",
+        #           seventh => "seven Swans-a-Swimming",
+        #           eighth => "eight Maids-a-Milking",
+        #           ninth => "nine Ladies Dancing",
+        #           tenth => "ten Lords-a-Leaping",
+        #           eleventh => "eleven Pipers Piping",
+        #           twelfth => "twelve Drummers Drumming"}
+
+        # lyrics.each do |day|
+
+
+                    # daily_gifts.each do |gift|
             #     if  == daily_gifts.last
             #         verse += ", and #{gift}."
             #     elsif
             #         verse += gift + ", "
             #     end
             # end
-        end
-    end
-
-
-
-
-    "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree."
-    # need base song
-    # need to get the number in to the day -> ordinalize
-    # need to build an hash that returns the gift {1 => Patridge}
-    12.times do |day|
-    
-
-end
