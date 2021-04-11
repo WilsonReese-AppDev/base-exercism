dc5c7e1b64535ea11a17cc7fea12cb0c725d344e
WilsonReese <reese@chicagobooth.edu>
2021-04-11 17:44:41 +0000

    added and a - need to make an array of verses

--- ruby/twelve-days/twelve_days.rb
+++ ruby/twelve-days/twelve_days.rb
@@ -6,23 +6,27 @@ To get started with TDD, see the `README.md` file in your
 `ruby/twelve-days` directory.
 =end
 
-class Integer
-    def initialize(value)
-        @value = value
-    end
-    
-    def self.ordinalize
-        ordinals = ["first","second","third","fourth","fifth","sixth","seventh","eighth","ninth","tenth","eleventh","twelfth"]
-        ordinals[(@value-1)]
-    end
-end
-
 class TwelveDays 
     def base_song(day = "first")
         @song = "On the #{day} day of Christmas my true love gave to me: "
     end
 
     def song
+        # lyrics = {first => "a Partridge in a Pear Tree",
+        #           second => "two Turtle Doves",
+        #           third => "three French Hens",
+        #           fourth => "four Calling Birds",
+        #           fifth => "five Gold Rings",
+        #           sixth => "six Geese-a-Laying",
+        #           seventh => "seven Swans-a-Swimming",
+        #           eighth => "eight Maids-a-Milking",
+        #           ninth => "nine Ladies Dancing",
+        #           tenth => "ten Lords-a-Leaping",
+        #           eleventh => "eleven Pipers Piping",
+        #           twelfth => "twelve Drummers Drumming"}
+
+        # lyrics.each do |day|
+            
         ordinals = ["first","second","third","fourth","fifth","sixth","seventh","eighth","ninth","tenth","eleventh","twelfth"]
         gifts = ["a Partridge in a Pear Tree",
                  "two Turtle Doves",
@@ -38,11 +42,20 @@ class TwelveDays
                  "twelve Drummers Drumming"]
         daily_gifts = Array.new
         (1..12).each do |day|
+            verse = ""
             daily_gifts = daily_gifts.prepend(gifts[day - 1])
-                daily_gifts.each do |gift|
-
-
-            @song = base_song(ordinals[day-1])
+            if daily_gifts.size > 1
+                daily_gifts[-1] = daily_gifts[-1].prepend("and ")
+            end
+            verse = daily_gifts.join(", ")
+            # daily_gifts.each do |gift|
+            #     if  == daily_gifts.last
+            #         verse += ", and #{gift}."
+            #     elsif
+            #         verse += gift + ", "
+            #     end
+            # end
+            @song = base_song(ordinals[day-1]) + @song + verse
         end
     end
 
